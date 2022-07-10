345e27a78bfaf421582cba584df1f3b4aafbacf1
Abdulla <abdullaravshanov@gmail.com>
2022-07-10 14:45:53 +0300

    First page

---
+++ begin/js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// console.log(obj.Name);
+
+// let arr = ['apple', 'orange', 9, 'fruit', [], {}];
+// console.log(arr[2]);
+
+// const arr = [1, 2, 3];
+// const arrObj = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c'
+// };
+
+// arrObj.b = '1234';
+// arrObj.w = '564321';
+
+// console.log(arrObj.w);
+// console.log(arrObj.b);
+
+// alert('hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+// const answer = +prompt('вам есть 18', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('whats your name', '');
+// answers[1] = prompt('whats your female', '');
+// answers[2] = prompt('whats your age', '');
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`http://someurl.com/${category}/5`);
+
+console.log('arr' + ' - object');
+console.log(4 + "5");
